f02b49ccb222fb256799bbadebe1fc4b5e96175e
Shubham <shubham4315@gmail.com>
2021-09-05 18:38:49 +0200

    added search bar and name suggestions div

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import Header from '../Header/Header';
+import ResultsContainer from '../ResultsContainer/ResultsContainer';
+import SearchBox from '../SearchBox/SearchBox';
 import './App.css';
+const name = require('@rstacruz/startup-name-generator');
 
 class App extends React.Component{
     state = {
-        headerText : "Hello, World!!"
+        headerText : "Name It!!",
+        headerExpanded : true,
+        suggestedNames  :[],
     };
+
+    handleInputChange = (inputText)=>{
+        this.setState({suggestedNames : inputText ? name(inputText):[]});
+        this.setState({headerExpanded:!inputText});
+    }
     render(){
         return(
             <div>
-                <Header/>
-                <h3>{this.state.headerText}</h3>
-                <button
-                onClick={()=>{
-                    this.setState({
-                        headerText : "woah it changed"
-                    });
-                }}
-                >
-                    Magic happens here</button>
+                <Header headTitle={this.state.headerText}
+                        headerExpanded={this.state.headerExpanded}
+                />
+                <SearchBox onInputChange={this.handleInputChange}/>
+                <ResultsContainer suggestedNames = {this.state.suggestedNames}/> 
             </div> 
         )
     }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,30 @@
-import React  from "react";
-import './Header.css';
+import React from 'react'
+import './Header.css'
 
-const header = ()=>{
-    return(
-         <div className="head-container">
-             <img src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png" className="head-image" alt="header-iamge"/>
-             <h1 className="head-text"> Name It!!</h1>
-         </div>
+const header = ({ headTitle, headerExpanded }) => {
+    return (
+        <div className="head-container">
+            <img
+                src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
+                className={`head-image 
+                ${
+                    headerExpanded
+                        ? 'head-image-expanded'
+                        : 'head-image-contracted'
+                }`}
+                alt="header-iamge"
+            />
+            <h1
+                className={`head-text 
+                ${
+                    headerExpanded
+                        ? 'head-text-expanded'
+                        : 'head-text-contracted'
+                }`}
+            >
+                {headTitle}
+            </h1>
+        </div>
     )
 }
-export default header;
+export default header
